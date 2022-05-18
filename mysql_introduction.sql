10567a8e408febbdc608f7f9e426ba022cabbd38
Meredith Wang <wang.meredith09@gmail.com>
2022-05-18 10:07:05 -0500

    changing exercise code

--- mysql_introduction.sql
+++ mysql_introduction.sql
@@ -1,4 +1,4 @@
-use employees;
-
-SELECT * FROM employees
-limit 10;
+SELECT * FROM mysql.user;
+SELECT user, host FROM mysql.user;
+SELECT * FROM mysql.help_topic;
+SELECT help_topic_id, help_category_id, url FROM mysql.help_topic;
